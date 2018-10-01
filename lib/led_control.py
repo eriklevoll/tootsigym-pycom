91dfdc331875c7f8d36edd4564e1c174daf2b85b
eriklev <erik.levoll@gmail.com>
2018-10-01 20:01:11 +0300

    minor changes

--- lib/led_control.py
+++ lib/led_control.py
@@ -112,6 +112,7 @@ class Control:
         else: i = self.get_index(rc) - 1
 
         if self.check_valid_data(i, r, g, b) == False: return
+        #print ("index: ", i)
         if i < 0:
             self.set_all_leds(r,g,b)
         else:
